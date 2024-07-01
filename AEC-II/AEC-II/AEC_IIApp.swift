740fe23841dd167607e1a0ca956341f329dfa078
Nishka Sharma <91435323+nishkasharma21@users.noreply.github.com>
2024-07-01 10:52:32 -0700

    added XCode proj

---
+++ AEC-II/AEC-II/AEC_IIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AEC_IIApp.swift
+//  AEC-II
+//
+//  Created by Nishka Sharma on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AEC_IIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
